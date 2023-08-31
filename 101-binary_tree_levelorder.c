a0f476697575210b8c77be03c893ea9bbd9526a7
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-08-31 08:51:49 -0700

    tree is complete

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -24,7 +24,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 }
 /**
- * binary_tree_depth - Function that returns the depth of specified node from root
+ * binary_tree_depth - Function that returns the depth of specified node
+ * from root
  * @tree: node to check the depth
  * Return: 0 is it is the root or number of depth
  */
