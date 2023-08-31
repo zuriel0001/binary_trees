c6b856196690f20529c1913b81be10ab4471c372
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-08-31 15:05:01 -0700

    updataed subtree checker

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
- * check_sub_tree_Left - Check if the binary tree is a valid left subtree.
+ * check_left_subtree - Check if the binary tree is a valid left subtree.
  *
  * @node: Pointer to the root of the subtree to be checked
  * @max: The maximum value that all nodes in the subtree must be smaller than.
  *
  * Return: 1 if the subtree is a valid left subtree, 0 otherwise.
  */
-int check_sub_tree_Left(const binary_tree_t *node, int max)
+int check_left_subtree(const binary_tree_t *node, int max)
 {
 	int left = 0, right = 0;
 
@@ -20,8 +20,8 @@ int check_sub_tree_Left(const binary_tree_t *node, int max)
 		if (node->n >= max)
 			return (0);
 
-		left = check_sub_tree_Left(node->left, max);
-		right = check_sub_tree_Left(node->right, max);
+		left = check_left_subtree(node->left, max);
+		right = check_left_subtree(node->right, max);
 
 		if (left == right && left == 1)
 			return (1);
@@ -30,14 +30,14 @@ int check_sub_tree_Left(const binary_tree_t *node, int max)
 	}
 }
 /**
- * check_sub_tree_Right - Check if the binary tree is a valid right subtree.
+ * check_right_subtree - Check if the binary tree is a valid right subtree.
  *
  * @node: Pointer to the root of the subtree to be checked.
  * @min: The minimum value that all nodes in the subtree must be greater than.
  *
  * Return: 1 if the subtree is a valid right subtree, 0 otherwise.
  */
-int check_sub_tree_Right(const binary_tree_t *node, int min)
+int check_right_subtree(const binary_tree_t *node, int min)
 {
 	int left = 0, right = 0;
 
@@ -49,8 +49,8 @@ int check_sub_tree_Right(const binary_tree_t *node, int min)
 	{
 		if (node->n <= min)
 			return (0);
-		left = check_sub_tree_Right(node->left, min);
-		right = check_sub_tree_Right(node->right, min);
+		left = check_right_subtree(node->left, min);
+		right = check_right_subtree(node->right, min);
 		if (left == right && left == 1)
 			return (1);
 		return (0);
@@ -82,14 +82,14 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 
 	if (tree->left && tree->left->n < tree->n)
 	{
-		valid_subtree = check_sub_tree_Left(tree->left, tree->n);
+		valid_subtree = check_left_subtree(tree->left, tree->n);
 		if (valid_subtree == 0)
 			return (0);
 		left = binary_tree_is_bst(tree->left);
 	}
 	if (tree->right && tree->right->n > tree->n)
 	{
-		valid_subtree = check_sub_tree_Right(tree->right, tree->n);
+		valid_subtree = check_right_subtree(tree->right, tree->n);
 		if (valid_subtree == 0)
 			return (0);
 		right = binary_tree_is_bst(tree->right);
