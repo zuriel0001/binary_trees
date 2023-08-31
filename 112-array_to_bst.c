0f0be9b7ba2d5001ab014f3013db93143f7f55f8
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-08-31 15:18:48 -0700

    V

---
+++ 112-array_to_bst.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * array_to_bst - a function that builds a Binary
+ *                Search Tree from an array
+ *
+ * @array: Pointer to the array of integers.
+ * @size: he number of elements in the array.
+ *
+ * Return: BST tree from array
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i = 0;
+	bst_t *bst_root;
+
+	root = NULL;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		if (i == 0)
+		{
+			bst_insert(&bst_root, array[i]);
+		}
+		else
+		{
+			bst_insert(&bst_root, array[i]);
+		}
+		i++;
+	}
+	return (bst_root);
+}
